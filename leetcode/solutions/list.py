a2c4606d0c4d754a1adde320fdb6389c37ddadf5
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-06-12 18:30:22 +0400

    LeetCode: Merge sorted arrays

--- leetcode/solutions/list.py
+++ leetcode/solutions/list.py
@@ -14,3 +14,28 @@ class ListSolution:
                 length += 1
 
         return length if length > max_length else max_length
+
+    # Merge Sorted Array
+    def mergeSortedArray(self, nums1, m: int, nums2, n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        nums1Copy = nums1[:m].copy()
+        i = 0
+        for _ in nums1:
+            p1 = nums1Copy[0] if nums1Copy else None
+            p2 = nums2[0] if nums2 else None
+            if p1 is not None and p2 is not None:
+                if p1 > p2:
+                    nums1[i] = p2
+                    nums2 = nums2[1:]
+                else:
+                    nums1[i] = p1
+                    nums1Copy = nums1Copy[1:]
+            elif p1 is not None:
+                nums1[i] = p1
+                nums1Copy = nums1Copy[1:]
+            else:
+                nums1[i] = p2
+                nums2 = nums2[1:]
+            i += 1
--- leetcode/tests/test_list.py
+++ leetcode/tests/test_list.py
@@ -4,8 +4,7 @@ import random
 from leetcode.solutions.list import *
 
 
-class TestLinkedListSolution(unittest.TestCase):
-
+class TestListSolution(unittest.TestCase):
     global s
     s = ListSolution()
 
@@ -15,3 +14,20 @@ class TestLinkedListSolution(unittest.TestCase):
         expected_results_list = [3, 1, 3, 0, 2, 3]
         for string, result in zip(test_list, expected_results_list):
             self.assertEqual(s.lengthOfLongestSubstring(string), result)
+
+    # Merge Sorted Array
+    def test_mergeSortedArray(self):
+        for _ in range(0, 100000):
+            lst1_len = random.randint(0, 10)
+            lst2_len = random.randint(0, 10)
+            lst1 = [random.randint(-100, 100) for _ in range(0, lst1_len)]
+            lst1.sort()
+            lst1 += [0] * lst2_len
+            lst2 = [random.randint(-100, 100) for _ in range(0, lst2_len)]
+            lst2.sort()
+            expected_result = lst1[:lst1_len] + lst2
+            expected_result.sort()
+            s.mergeSortedArray(lst1, lst1_len, lst2, lst2_len)
+            self.assertEqual(expected_result, lst1)
+
+
