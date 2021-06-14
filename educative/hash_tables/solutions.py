3bbb2e5e50b084ee2bab40600b7489a37aac807e
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-06-14 07:51:36 -0700

    Educative: disjoint lists & trace the path of a journey

--- educative/hash_tables/solutions.py
+++ educative/hash_tables/solutions.py
@@ -14,3 +14,32 @@ def is_disjoint(list1, list2):
             return False
 
     return True
+
+
+# Challenge 3: Check if Lists are Disjoint
+def find_symmetric(my_list):
+    dct = dict(my_list)
+    out = []
+
+    for val in dct.values():
+        if dct.get(val) is not None:
+            out.append([dct[val], val])
+
+    return out
+
+
+# Challenge 4: Trace the Complete Path of a Journey
+def trace_path(my_dict: dict):
+    set_dests = set(my_dict.values())
+    out = []
+    for k in my_dict.keys():
+        if k not in set_dests:
+            break
+
+    while my_dict.get(k):
+        pair = [k, my_dict[k]]
+        out.append(pair)
+        k = pair[1]
+
+    return out
+
--- educative/hash_tables/tests.py
+++ educative/hash_tables/tests.py
@@ -14,3 +14,24 @@ class TestHashTableSolution(unittest.TestCase):
         list1 = [9, 4, 3, 1, -2, 6, 5]
         list2 = [7, 10, 8]
         self.assertTrue(is_disjoint(list1, list2))
+
+    def test_challenge_3(self):
+        list1 = [[1, 2], [3, 4], [5, 9], [4, 3], [9, 5]]
+        expected_result = [[3, 4], [5, 9], [4, 3], [9, 5]]
+        function_output = find_symmetric(list1)
+        self.assertTrue(expected_result, function_output)
+
+    def test_challenge_4(self):
+        dct = {
+            "NewYork": "Chicago",
+            "Boston": "Texas",
+            "Missouri": "NewYork",
+            "Texas": "Missouri"
+        }
+        function_output = trace_path(dct)
+        expected_result = [['Boston', 'Texas'],
+                           ['Texas', 'Missouri'],
+                           ['Missouri', 'NewYork'],
+                           ['NewYork', 'Chicago']]
+        self.assertTrue(expected_result, function_output)
+
