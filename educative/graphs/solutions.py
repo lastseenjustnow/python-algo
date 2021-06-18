116c9cd55a398ade617217e87fec8b3180903758
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-06-18 14:24:24 +0400

    Educative: Depth First Search, recursive approach & stack

--- educative/graphs/solutions.py
+++ educative/graphs/solutions.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from Graph import Graph
 from educative.linked_lists.LinkedList import LinkedList, Node
 from educative.stacks_n_queues.Queue import MyQueue
-
+from educative.stacks_n_queues.Stack import MyStack
 
 # Challenge 1: Implement Breadth First Search
 def bfs_traversal(g: Graph, source: int):
@@ -21,3 +23,54 @@ def bfs_traversal(g: Graph, source: int):
         q.dequeue()
 
     return result
+
+
+# Challenge 2: Depth First Search
+# Recursive approach
+def dfs_traversal_recursive(g: Graph, source: int) -> str:
+
+    def rec(vertex: int, t: List[bool], res: str):
+        if not t[vertex]:
+            res += str(vertex)
+            t[vertex] = True
+            vertex_head = g.array[vertex].get_head()
+            while vertex_head:
+                res = rec(vertex_head.data, t, res)[0]
+                vertex_head = vertex_head.next_element
+
+        return res, t
+
+    result, traversed = rec(source, [False] * g.vertices, "")
+
+    for i, b in enumerate(traversed, 0):
+        if not b:
+            result += rec(i, traversed, "")[0]
+
+    return result
+
+
+# Stack approach
+def dfs_traversal(g: Graph, source: int) -> str:
+
+    is_traversed = [False] * g.vertices
+
+    res = ""
+    stack = MyStack()
+    stack.push(source)
+
+    while not stack.is_empty():
+        current_vertex_id = stack.pop()
+        res += str(current_vertex_id)
+        is_traversed[current_vertex_id] = True
+        current_vertex_head: Node = g.array[current_vertex_id].get_head()
+        while current_vertex_head:
+            if not is_traversed[current_vertex_head.data]:
+                stack.push(current_vertex_head.data)
+            current_vertex_head = current_vertex_head.next_element
+        if stack.is_empty():
+            for disjoined_vertex_id, is_visited in enumerate(is_traversed, 0):
+                if not is_visited:
+                    stack.push(disjoined_vertex_id)
+                    break
+
+    return res
--- educative/graphs/tests.py
+++ educative/graphs/tests.py
@@ -13,3 +13,22 @@ class TestGraphSolution(unittest.TestCase):
         expected_result = "02143"
         function_output = bfs_traversal(graph, 0)
         self.assertEqual(expected_result, function_output)
+
+    # Challenge 2: Depth First Search
+    def test_dfs_traversal_recursive(self):
+        graph = Graph(7)
+        source_destination = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6)]
+        for x in source_destination:
+            graph.add_edge(x[0], x[1])
+        expected_result = "1362540"
+        function_output = dfs_traversal_recursive(graph, 1)
+        self.assertEqual(expected_result, function_output)
+
+    def test_dfs_traversal(self):
+        graph = Graph(7)
+        source_destination = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6)]
+        for x in source_destination:
+            graph.add_edge(x[0], x[1])
+        expected_result = "1245360"
+        function_output = dfs_traversal(graph, 1)
+        self.assertEqual(expected_result, function_output)
