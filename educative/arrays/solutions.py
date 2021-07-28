eace7c065b8df242cfcdff905d66025ecab46f6f
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-07-28 14:59:28 +0400

    Educative: Search a Rotated Array

--- educative/arrays/solutions.py
+++ educative/arrays/solutions.py
@@ -1,6 +1,7 @@
 from collections import deque
 
 
+# Find Maximum in Sliding Window
 def find_max_sliding_window(arr, window_size):
     result = []
     window = deque()
@@ -17,3 +18,50 @@ def find_max_sliding_window(arr, window_size):
             continue
 
     return result
+
+
+# Search a Rotated Array
+# Recursive approach
+def binary_search_rotated(arr, key):
+    def rec(left_index, right_index):
+        middle_index = (right_index - left_index) // 2 + left_index
+        if key == arr[left_index] or key == arr[right_index]:
+            return left_index if arr[left_index] == key else right_index
+        elif arr[left_index] < arr[right_index] and key < arr[middle_index]:
+            return rec(left_index, middle_index)
+        elif arr[left_index] < arr[right_index] and key > arr[middle_index]:
+            return rec(middle_index + 1, right_index)
+        elif arr[middle_index] < arr[right_index] and arr[middle_index] < key <= arr[right_index]:
+            return rec(middle_index + 1, right_index)
+        elif arr[middle_index] > arr[right_index] and not arr[left_index] < key <= arr[middle_index]:
+            return rec(middle_index + 1, right_index)
+        else:
+            return rec(left_index, middle_index)
+
+    return rec(0, len(arr)-1)
+
+
+# Iterative approach
+
+# Important!
+# More efficient than recursive in Python because both algorithms could be executed over the same time,
+# but iterative consumes O(1) memory, while recursive consumes O(log n) as it keeps recursive call stack in memory
+# JVM implementations may neutralize that effect.
+def binary_search_rotated_iter(arr, key):
+
+    left_index, right_index = 0, len(arr)-1
+
+    while arr[left_index] != key and arr[right_index] != key:
+        middle_index = (right_index - left_index) // 2 + left_index
+        if arr[left_index] < arr[right_index] and key < arr[middle_index]:
+            right_index = middle_index
+        elif arr[left_index] < arr[right_index] and key > arr[middle_index]:
+            left_index = middle_index + 1
+        elif arr[middle_index] < arr[right_index] and arr[middle_index] < key <= arr[right_index]:
+            left_index = middle_index + 1
+        elif arr[middle_index] > arr[right_index] and not arr[left_index] < key <= arr[middle_index]:
+            left_index = middle_index + 1
+        else:
+            right_index = middle_index
+
+    return left_index if arr[left_index] == key else right_index
--- educative/arrays/tests.py
+++ educative/arrays/tests.py
@@ -1,12 +1,41 @@
 import unittest
+import random
 
 from solutions import *
 
 
 class TestArraySolution(unittest.TestCase):
+    # Find Maximum in Sliding Window
     def test_find_max_sliding_window(self):
         arr = [-4, 2, -5, 3, 6]
         self.assertTrue([2, 3, 6], find_max_sliding_window(arr, 3))
 
         arr = [-4, 2, -5, 1, -1, 6]
         self.assertTrue([2, 2, 1, 6], find_max_sliding_window(arr, 3))
+
+    # Search a Rotated Array
+    def test_binary_search_rotated(self):
+        arr = [176, 188, 199, 200, 210, 222, 1, 10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155, 162]
+        for index, elem in enumerate(arr, 0):
+            self.assertEqual(index, binary_search_rotated(arr, elem))
+
+        for _ in range(100):
+            lst_len = random.randint(1, 100)
+            rotate_arbitrary_number = random.randint(1, lst_len)
+            lst = sorted(list(set([random.randint(-100, 100) for _ in range(lst_len)])))
+            lst = lst[rotate_arbitrary_number+1:] + lst[:rotate_arbitrary_number]
+            for index, elem in enumerate(lst, 0):
+                self.assertEqual(index, binary_search_rotated(lst, elem))
+
+    def test_binary_search_rotated_iter(self):
+        arr = [176, 188, 199, 200, 210, 222, 1, 10, 20, 47, 59, 63, 75, 88, 99, 107, 120, 133, 155, 162]
+        for index, elem in enumerate(arr, 0):
+            self.assertEqual(index, binary_search_rotated_iter(arr, elem))
+
+        for _ in range(100):
+            lst_len = random.randint(1, 100)
+            rotate_arbitrary_number = random.randint(1, lst_len)
+            lst = sorted(list(set([random.randint(-100, 100) for _ in range(lst_len)])))
+            lst = lst[rotate_arbitrary_number+1:] + lst[:rotate_arbitrary_number]
+            for index, elem in enumerate(lst, 0):
+                self.assertEqual(index, binary_search_rotated_iter(lst, elem))
