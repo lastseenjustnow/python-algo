cfb56303dd4b58030c48dd4414c1bd70b45ea494
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-08-16 15:25:40 +0400

    Educative: Algorithms. Greedy Algorithms. Challenge 2: Connecting n Pipes with Minimum Cost

--- educative/algorithms/greedy_algorithms/solutions.py
+++ educative/algorithms/greedy_algorithms/solutions.py
@@ -1,3 +1,5 @@
+import heapq
+
 # Challenge 1: Counting Money!
 
 def find_min_coins(v, coins_available):
@@ -25,3 +27,25 @@ def find_min_coins(v, coins_available):
             return stack
 
     return []
+
+
+# Challenge 2: Connecting n Pipes with Minimum Cost
+def min_cost(pipes):
+    """
+    Calculates the minimum cost of connecting pipes
+
+    Time: O(n log n)
+
+    :param pipes: A list where its length is the number of pipes and indexes are the specific lengths of the pipes.
+    :return: The minimum cost
+    """
+    result = 0
+    heapq.heapify(pipes)
+
+    while len(pipes) != 1:
+        mins = heapq.nsmallest(2, pipes)
+        result += sum(mins)
+        heapq.heappop(pipes)
+        heapq.heappushpop(pipes, sum(mins))
+
+    return result
--- educative/algorithms/greedy_algorithms/tests.py
+++ educative/algorithms/greedy_algorithms/tests.py
@@ -7,3 +7,7 @@ class TestGreedyAlgorithmSolution(unittest.TestCase):
     def test_find_min_coins(self):
         available_coins = [1, 5, 10, 25]
         self.assertEqual([10, 5, 1, 1, 1, 1], find_min_coins(19, available_coins))
+
+    def test_min_cost(self):
+        self.assertEqual(29, min_cost([4, 3, 2, 6]))
+        self.assertEqual(16, min_cost([1, 1, 2, 6]))
