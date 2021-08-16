7a4801088231977fb2b071d128bdd362858cb98d
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-08-16 17:40:04 +0400

    Educative: Algorithms. Greedy Algorithms. Challenge 3: Find the Egyptian Fraction's Denominators

--- educative/algorithms/greedy_algorithms/solutions.py
+++ educative/algorithms/greedy_algorithms/solutions.py
@@ -1,7 +1,7 @@
 import heapq
+import math
 
 # Challenge 1: Counting Money!
-
 def find_min_coins(v, coins_available):
     """
     This function finds the minimum number of coins
@@ -47,5 +47,33 @@ def min_cost(pipes):
         result += sum(mins)
         heapq.heappop(pipes)
         heapq.heappushpop(pipes, sum(mins))
+
+    return result
+
+
+# Challenge 3: Find the Egyptian Fraction's Denominators
+def egyptian_fraction(numerator, denominator):
+    """
+    Finds the egyptian fraction denominators
+    :param numerator: Numerator of the fraction
+    :param denominator: Denominator of the fraction
+    :return: A list of denominators of the egyptian fraction
+    """
+
+    gcd = math.gcd(numerator, denominator)
+    numerator = int(numerator / gcd)
+    denominator = int(denominator / gcd)
+
+    result, fr = [], 1
+
+    while numerator != 1:
+        if 1 / fr < numerator / denominator:
+            result.append(fr)
+            greatest_common_factor: int = int(fr * denominator / math.gcd(fr, denominator))
+            new_numerator: int = numerator * greatest_common_factor / denominator - greatest_common_factor / fr
+            new_denominator: int = greatest_common_factor
+            numerator, denominator = new_numerator, new_denominator
+        fr += 1
+    result.append(denominator)
 
     return result
--- educative/algorithms/greedy_algorithms/tests.py
+++ educative/algorithms/greedy_algorithms/tests.py
@@ -4,10 +4,18 @@ from solutions import *
 
 
 class TestGreedyAlgorithmSolution(unittest.TestCase):
+    # Challenge 1: Counting Money!
     def test_find_min_coins(self):
         available_coins = [1, 5, 10, 25]
         self.assertEqual([10, 5, 1, 1, 1, 1], find_min_coins(19, available_coins))
 
+    # Challenge 2: Connecting n Pipes with Minimum Cost
     def test_min_cost(self):
         self.assertEqual(29, min_cost([4, 3, 2, 6]))
         self.assertEqual(16, min_cost([1, 1, 2, 6]))
+
+    # Challenge 3: Find the Egyptian Fraction's Denominators
+    def test_egyptian_fraction(self):
+        self.assertEqual([2, 6], egyptian_fraction(2, 3))
+        self.assertEqual([2, 8], egyptian_fraction(5, 8))
+        self.assertEqual([3, 11, 231], egyptian_fraction(6, 14))
