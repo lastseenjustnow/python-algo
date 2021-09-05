9db938a7bfd91005ab76d22291bac6c3c0b5b0c0
lastseenjustnow <lastseenjustnow@protonmail.com>
2021-09-05 18:59:32 +0400

    Educative: Algorithms. Divide & Conquer Challenge 4: Collect Coins in Minimum Steps Challenge 5: Find the Floor and Ceil of a Number in a Sorted List Challenge 7: Find the Closest Number

--- educative/algorithms/divide_n_conquer/solutions.py
+++ educative/algorithms/divide_n_conquer/solutions.py
@@ -47,3 +47,105 @@ def max_sub_list_of_size_k(lst, k):
 
     return glob_max
 
+
+# Challenge 4: Collect Coins in Minimum Steps
+def minimum_steps(lst):
+    """
+    Function which calculates the minimum steps to collect coins from the list
+    :param lst: List of coins stack
+    :return: Returns minimum steps to collect coins from the list, otherwise 0
+    """
+    if len(lst) == 1:
+        return 1
+    if len(lst) == 0:
+        return 0
+
+    max_elem = max(lst)
+    if len(lst) > max_elem:
+        lists, new_list = [], []
+        for elem in lst:
+            if elem - 1 == 0:
+                lists.append(new_list)
+                new_list = []
+            else:
+                new_list.append(elem - 1)
+        return sum([minimum_steps(x) for x in lists]) + 1
+
+    return minimum_steps(lst[:lst.index(max_elem)]) \
+           + minimum_steps(lst[lst.index(max_elem)+1:]) \
+           + 1
+
+
+# Challenge 5: Find the Floor and Ceil of a Number in a Sorted List
+def find_floor(lst, low, high, x):
+    """
+    Modified binary search function to find the floor of given number x
+    :param lst: List of integers
+    :param low: Starting index of the list
+    :param high: Ending index of the list
+    :return: Returns the floor of an integer x if exists, otherwise -1
+    """
+    middle = (high - low) // 2 + low
+    if middle + 1 == len(lst):
+        return lst[middle]
+    if x > lst[middle]:
+        if x <= lst[middle+1]:
+            return lst[middle]
+        return find_floor(lst, middle + 1, high, x)
+    return find_floor(lst, low, middle, x)
+
+
+def find_ceiling(lst, low, high, x):
+    """
+    Modified binary search function to find the floor of given number x
+
+    Time: O(log n)
+
+    :param lst: List of integers
+    :param low: Starting index of the list
+    :param high: Ending index of the list
+    :return: Returns the ceiling of an integer x if exists, otherwise -1
+    """
+
+    middle = (high - low) // 2 + low
+    if middle + 1 == len(lst):
+        return -1
+    if x >= lst[middle]:
+        if x <= lst[middle+1]:
+            return lst[middle+1]
+        return find_ceiling(lst, middle + 1, high, x)
+    return find_ceiling(lst, low, middle, x)
+
+
+def find_floor_ceiling(lst, x):
+    # DO NOT MODIFY THIS FUNCTION #
+
+    """
+    Calls the find_floor and find_ceiling functions and returns their results
+    :param lst: List of integers
+    :param x: An integer
+    :return: Returns the floor of an integer x, otherwise -1
+    """
+    return find_floor(lst, 0, len(lst) - 1, x), find_ceiling(lst, 0, len(lst) - 1, x)
+
+
+# Challenge 7: Find the Closest Number
+def find_closest(lst, target):
+    """
+    Finds the closest number to the target in the list
+    :param lst: Sorted list of integers
+    :param target: Left sided index of the list
+    :return: Closest element from the list to the target
+    """
+
+    def rec(start, end):
+        mid = (end - start) // 2 + start
+        if mid == len(lst) - 1:
+            return lst[mid]
+        if abs(lst[mid] - target) > abs(lst[mid+1] - target):
+            return rec(mid + 1, end)
+        if abs(lst[mid] - target) > abs(lst[mid-1] - target):
+            return rec(start, mid)
+        return lst[mid]
+
+    return rec(0, len(lst) - 1)
--- educative/algorithms/divide_n_conquer/tests.py
+++ educative/algorithms/divide_n_conquer/tests.py
@@ -18,3 +18,26 @@ class TestDivideNConquerAlgorithmsSolution(unittest.TestCase):
     def test_max_sub_list_of_size_k(self):
         self.assertEqual(9, max_sub_list_of_size_k([2, 1, 5, 1, 3, 2], 3))
         self.assertEqual(7, max_sub_list_of_size_k([2, 3, 4, 1, 5], 2))
+
+    # Challenge 4: Collect Coins in Minimum Steps
+    def test_minimum_steps(self):
+        lst = [2, 5, 1, 2, 3, 1]
+        self.assertEqual(5, minimum_steps(lst))
+        lst = [2, 1, 2, 5, 1]
+        self.assertEqual(4, minimum_steps(lst))
+
+    # Challenge 5: Find the Floor and Ceil of a Number in a Sorted List
+    def test_find_floor_ceiling(self):
+        lst = [1, 2, 3, 5, 7]
+        self.assertEqual((2, 5), find_floor_ceiling(lst, 3))
+        lst = [0, 1, 2, 5]
+        self.assertEqual((5, -1), find_floor_ceiling(lst, 6))
+        lst = [1, 2, 3, 5, 7]
+        self.assertEqual((3, 5), find_floor_ceiling(lst, 4))
+
+    # Challenge 7: Find the Closest Number
+    def test_find_closest(self):
+        lst = [-9, -4, -2, 0, 1, 3, 4, 10]
+        self.assertEqual(4, find_closest(lst, 5))
+        lst = [1, 2, 5, 10, 23, 25, 30, 50]
+        find_closest(lst, 100)
